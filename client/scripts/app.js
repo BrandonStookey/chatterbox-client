0f335c276d1b5697321654a6cbd233c4a7f4f112
Brandon Stookey <bwstookey@gmail.com>
2016-01-26 14:33:28 -0600

    Added room, Displayed Messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,11 +1,13 @@
 // YOUR CODE HERE:
-
 var app = {
+  server: "https://api.parse.com/1/classes/chatterbox/",
+
   friends: [],
+
   init: function() {},
 
   send: function(message) {
-    return $.ajax({
+    $.ajax({
         url: 'https://api.parse.com/1/classes/chatterbox',
         type: 'POST',
         data: JSON.stringify(message),
@@ -20,60 +22,80 @@ var app = {
   },
 
   fetch: function(){
-    return $.ajax({
-        url: app.server,
-        type: 'GET',
-        data: JSON.stringify(message),
-        contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent. Data: ', data);
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to send message. Error: ', data);
+    var currentDate = new Date().toISOString();
+    $.ajax({ 
+      type: 'GET', 
+      url: this.server, 
+      data: {
+        where: JSON.stringify({
+          createdAt: {
+            $gte: {
+              __type: "Date", 
+              iso: '2016-01-26T05:00:00.000Z'
+            }
+          }
+        }),
+        limit: 20, 
+        order: "createdAt"}, 
+      dataType: 'json',
+      success: function (data){
+        console.log(data);
+        _.each(data.results, function(message) {
+          app.addMessage(message);
+        });
       }
     });
   },
-  //if anythings ever breaks, it might be me
+
   clearMessages: function(){
-    $('#chats').empty();
+    $('.chat').empty();
   },
 
-  addMessage: function(message) {
-    $("#chats").append('<div class="messages"><a href="#" class="username">' + message.username + '</a>: ' + message.text + '</div>');
+  addMessage: function(message){
+    message.text = message.text || message.message;
+    var timeStamp = new Date(message.createdAt);
+    var date = timeStamp.getDate();
+    var month = ' ' + (timeStamp.getMonth() + 1) + '/';
+    var time = timeStamp.toLocaleTimeString();
+    $(".chat").prepend('<div class="messages"><a href="#" class="username">' + message.username + '</a> ' + month + date + ' at ' + time + '<br>' + message.text + '</div>');
   },
 
-  addRoom: function(room) {
-    $("#roomSelect").append('<div class="rooms">' + room + '</div>');
+  addRoom: function(roomName){
+    $("#roomSelect").append('<option>' + roomName + '</option>');
   },
 
   addFriend: function(name){
     this.friends.push(name);
+  },
+
+  handleSubmit: function(message){
+    this.send(message);
   }
 
 };
 
 $(document).ready(function() {
   $("body").on('click', '.username', function(e){
-    console.log("I have been clicked!");
-    console.log(e);
     app.addFriend(this.innerHTML);
+  });
+
+  $("body").on('submit', function(e){
+    console.log('clicked');
+    app.handleSubmit($('#message').val());
+  });
+
+  $("body").on('click', '.updates', function(){
+    app.fetch();
+  });
+
+  $("body").on('click', '.addRoom', function(e){
+    var room = $('input.roomField').val();
+    app.addRoom(room);
+    $('input.roomField').val("");
   });
 });
 
-// $("body").on("click", '.squareDancer', function(event){
-//      //$(this).css("background-size", "1120px");
-//     var topValue =$(this).position().top;
-//     var leftValue = $(this).position().left;
-//     var maxleft = $(this).position().left+250; //200     
-//     var minleft = $(this).position().left-150;  //0
-//     var maxtop = $(this).position().top+200;  //200
-//     var mintop = $(this).position().top-200;  //0
 
-//    // console.log(topValue, leftValue, maxleft,minleft,maxtop,mintop);
-//     console.log($(this));
-//     var arr = [];
-//     var move = function(x){
-//     // this.oldStep();
-//     // this.$node.toggle();
-//      $(x).addClass('animated infinite bounce');
-//     };
+
+
+
